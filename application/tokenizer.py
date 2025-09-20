ad441b6a1b00b38bc58e43cb4e45fb29633034b9
Rohit Bhatt <114086101+rohitbhatt03@users.noreply.github.com>
2025-09-20 11:04:16 +0530

    Update tokenizer.py

--- application/tokenizer.py
+++ application/tokenizer.py
@@ -1,6 +1,5 @@
 import tiktoken
 
-
 def num_tokens_from_string(string: str, model: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.encoding_for_model(model)
